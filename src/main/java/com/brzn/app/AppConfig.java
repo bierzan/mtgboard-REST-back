46d3116f4ae3df6d2cfe01917263e4534c2ddddb
bierzan@gmail.com <bierzan@gmail.com>
2019-03-26 23:08:00 +0100

    appconfig and init

---
+++ src/main/java/com/brzn/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.brzn.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.brzn")
+public class AppConfig extends WebMvcConfigurerAdapter {
+}
