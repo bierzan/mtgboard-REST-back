94f8c9153031df9e969891ebfe2d7cb0caec9717
bierzan@gmail.com <bierzan@gmail.com>
2019-03-27 00:29:56 +0100

    cards set Dto class

---
+++ src/main/java/com/brzn/cardsSet/Expansion.java
@@ -0,0 +1,30 @@
+package com.brzn.cardsSet;
+
+enum Expansion {
+    CORE("core"),
+    EXPANSION("expansion"),
+    REPRINT("reprint"),
+    BOX("box"),
+    UN("un"),
+    FROM_THE_VAULT("from the vault"),
+    PREMIUM_DECK("premium deck"),
+    DUEL_DECK("duel deck"),
+    STARTER("starter"),
+    COMMANDER("commander"),
+    PLANECHASE("planechase"),
+    ARCHENEMY("archenemy"),
+    PROMO("promo"),
+    VANGUARD("vanguard"),
+    MASTERS("masters");
+
+    private String typeName;
+
+    Expansion(String typeName) {
+        this.typeName = typeName;
+    }
+
+    private String getExpansionName(){
+        return this.typeName;
+    }
+
+}
