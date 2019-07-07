b3d00ec9f8afa9343a8dfc491a357477edd8c4f9
bierzan@gmail.com <bierzan@gmail.com>
2019-07-08 00:25:11 +0200

    global cors

--- src/main/java/com/brzn/mtgboard/AppConfig.java
+++ src/main/java/com/brzn/mtgboard/AppConfig.java
@@ -11,18 +11,4 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(value = "com.brzn.mtgboard")
 public class AppConfig {
 
-//    @Bean
-//    public SimpleMailMessage templateSimpleMessage() {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setText(
-//                "Wiadomość od: %s, %s\n" +
-//                        "do: %s, %s\n" +
-//                        "Dotyczy oferty:\n" +
-//                        "%s\n\n" +
-//                        "Treść wiadomości:\n" +
-//                        "%s\n" +
-//                        "\n\n" +
-//                        "Wiadomość wygenerowana automatycznie przez serwis MTGBOARD");
-//        return message;
-//    }
 }
--- src/main/java/com/brzn/mtgboard/MtgBoardApplication.java
+++ src/main/java/com/brzn/mtgboard/MtgBoardApplication.java
@@ -1,9 +1,7 @@
 package com.brzn.mtgboard;
 
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MtgBoardApplication {
